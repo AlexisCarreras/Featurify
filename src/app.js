850c4b26459633391e7080cd9137102d6c50df36
Alexis <alexiscarreras@hotmail.com>
2024-06-03 18:00:22 -0300

    chore: servicios de search tracks y get track

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
@@ -59,19 +59,151 @@ app.get("/callback", (req, res) => {
     });
 });
 
+// => Search Track: API para obtener la información de una serie de tracks buscado por su nombre, por default selecionamos 10 resultados y que se busquen solo tracks.
 app.get("/search", (req, res) => {
   const { q } = req.query;
   spotifyApi
-    .searchTracks(q)
+    .searchTracks(q, { limit: 5 })
     .then((searchData) => {
-      const trackUri = searchData.body.tracks.items[0].uri;
-      res.send({ uri: trackUri });
+      res.send({
+        limit: searchData.body.tracks.limit,
+        items: [
+          {
+            idTrack: searchData.body.tracks.items[0].id,
+            type: searchData.body.tracks.items[0].type,
+            nameTrack: searchData.body.tracks.items[0].name,
+            durationMs: searchData.body.tracks.items[0].duration_ms,
+            explicit: searchData.body.tracks.items[0].explicit,
+            artists: [
+              {
+                idArtist: searchData.body.tracks.items[0].artists[0].id,
+                nameArtist: searchData.body.tracks.items[0].artists[0].name,
+              },
+            ],
+            album: {
+              idAlbum: searchData.body.tracks.items[0].album.id,
+              nameAlbum: searchData.body.tracks.items[0].album.name,
+              imageUrl: searchData.body.tracks.items[0].album.images[0].url
+            },
+          },
+          {
+            idTrack: searchData.body.tracks.items[1].id,
+            type: searchData.body.tracks.items[1].type,
+            nameTrack: searchData.body.tracks.items[1].name,
+            durationMs: searchData.body.tracks.items[1].duration_ms,
+            explicit: searchData.body.tracks.items[1].explicit,
+            artists: [
+              {
+                idArtist: searchData.body.tracks.items[1].artists[0].id,
+                nameArtist: searchData.body.tracks.items[1].artists[0].name,
+              },
+            ],
+            album: {
+              idAlbum: searchData.body.tracks.items[1].album.id,
+              nameAlbum: searchData.body.tracks.items[1].album.name,
+              imageUrl: searchData.body.tracks.items[1].album.images[0].url
+            },
+          },
+          {
+            idTrack: searchData.body.tracks.items[2].id,
+            type: searchData.body.tracks.items[2].type,
+            nameTrack: searchData.body.tracks.items[2].name,
+            durationMs: searchData.body.tracks.items[2].duration_ms,
+            explicit: searchData.body.tracks.items[2].explicit,
+            artists: [
+              {
+                idArtist: searchData.body.tracks.items[2].artists[0].id,
+                nameArtist: searchData.body.tracks.items[2].artists[0].name,
+              },
+            ],
+            album: {
+              idAlbum: searchData.body.tracks.items[2].album.id,
+              nameAlbum: searchData.body.tracks.items[2].album.name,
+              imageUrl: searchData.body.tracks.items[2].album.images[0].url
+            },
+          },
+          {
+            idTrack: searchData.body.tracks.items[3].id,
+            type: searchData.body.tracks.items[3].type,
+            nameTrack: searchData.body.tracks.items[3].name,
+            durationMs: searchData.body.tracks.items[3].duration_ms,
+            explicit: searchData.body.tracks.items[3].explicit,
+            artists: [
+              {
+                idArtist: searchData.body.tracks.items[3].artists[0].id,
+                nameArtist: searchData.body.tracks.items[3].artists[0].name,
+              },
+            ],
+            album: {
+              idAlbum: searchData.body.tracks.items[3].album.id,
+              nameAlbum: searchData.body.tracks.items[3].album.name,
+              imageUrl: searchData.body.tracks.items[3].album.images[0].url
+            },
+          },
+          {
+            idTrack: searchData.body.tracks.items[4].id,
+            type: searchData.body.tracks.items[4].type,
+            nameTrack: searchData.body.tracks.items[4].name,
+            durationMs: searchData.body.tracks.items[4].duration_ms,
+            explicit: searchData.body.tracks.items[4].explicit,
+            artists: [
+              {
+                idArtist: searchData.body.tracks.items[4].artists[0].id,
+                nameArtist: searchData.body.tracks.items[4].artists[0].name,
+              },
+            ],
+            album: {
+              idAlbum: searchData.body.tracks.items[4].album.id,
+              nameAlbum: searchData.body.tracks.items[4].album.name,
+              imageUrl: searchData.body.tracks.items[4].album.images[0].url
+            },
+          },
+        ],
+      });
     })
     .catch((err) => {
       res.send(`Error searching: ${err}`);
     });
 });
 
+//   => Get Track: API para obtener todos los detalles del track buscado, una vez que usamos la api de search, obtenemos el ID del Track solicitado, debemos guardarlo y pasárselo a este servicio a través del Front.
+app.get("/track", (req, res) => {
+  const { q } = req.query;
+  spotifyApi
+    .getTrack(q)
+    .then((trackData) => {
+      res.send({
+        idTrack: trackData.body.id,
+        type: trackData.body.type,
+        nameTrack: trackData.body.name,
+        previewTrackUrl: trackData.body.preview_url,
+        durationMs: trackData.body.duration_ms,
+        trackNumber: trackData.body.track_number,
+        explicit: trackData.body.explicit,
+        artists: [
+          {
+            idArtist: trackData.body.artists[0].id,
+            nameArtist: trackData.body.artists[0].name,
+            type: trackData.body.artists[0].type,
+          },
+        ],
+        album: {
+          idAlbum: trackData.body.album.id,
+          type: trackData.body.album.type,
+          typeAlbum: trackData.body.album.album_type,
+          nameAlbum: trackData.body.album.name,
+          totalTracks: trackData.body.album.total_tracks,
+          releaseDate: trackData.body.album.release_date,
+          imageUrl: trackData.body.album.images[0].url
+        },
+      });
+    })
+    .catch((err) => {
+      res.send(`Error searching: ${err}`);
+    });
+});
+
+// PRUEBA, únicamente para reproducir la pista en la APP
 app.get("/play", (req, res) => {
   const { uri } = req.query;
   spotifyApi
